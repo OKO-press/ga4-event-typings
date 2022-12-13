9f6f780dd716c4f90de4e9f9411120f5cccf9e84
Piotr Hylicki <hylickipiotr@gmail.com>
2022-12-13 14:56:57 +0100

    (OKO-847) Added banner types

---
+++ events/banner.d.ts
@@ -0,0 +1,45 @@
+/**
+ * Type of banner
+ */
+export type BannerType =
+  | "main-site-newest"
+  | "main-site-section-divider"
+  | "article-after-text"
+  | "article-after-lead"
+  | "article-inner-text";
+
+interface Item {
+  /**
+   * The type of payment method.
+   */
+  item_id: string;
+
+  /**
+   * The current campaign type with prefix.
+   * 
+   * @example "wesprzyj-cykliczna"
+   */
+  item_list_id: string;
+
+  /**
+   * The unique name of banner in slug case.
+   * 
+   * @example "this-is-example-banner"
+   */
+  creative_name: string;
+
+  /**
+   * The type of banner based on `BannerType`
+   * 
+   * @type BannerType
+   * @example "main-site-newest"
+   */
+  creative_slot: string;
+}
+
+export interface DataLayerBanner {
+  event: "view_promotion";
+  ecommerce: {
+    items: Item[];
+  }
+}
